1da666fd5afe5ca1b3883686e22108eb86c27386
Kyle Jones <kylejonesnh@gmail.com>
2020-01-16 14:51:44 -0500

    added orm.js file

---
+++ config/orm.js
@@ -0,0 +1,33 @@
+const connection = require("./connection");
+
+const orm = {
+    selectAll: (table, callback) => {
+        const query = "SELECT * FROM ??;";
+        connection.query(query, [table], (err, result) => {
+            if (err){
+                throw err;
+            }
+            callback(result);
+        });
+    },
+    insertOne: (table, data, callback) => {
+        const query = "INSERT INTO ?? SET ?;";
+        connection.query(query, [table, data], (err, result) => {
+            if (err){
+                throw err;
+            }
+            callback(result);
+        });
+    },
+    updateOne: (table, data, id, callback) => {
+        const query = "UPDATE ?? SET ? WHERE id = ?;";
+        connection.query(query, [table, data, id], (err, result) => {
+            if (err){
+                throw err;
+            }
+            callback(result);
+        });
+    }
+};
+
+module.exports = orm;
