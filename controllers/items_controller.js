a0e5124a2097aaa0797b6f7da25f7f8a340c156b
Kyle Jones <kylejonesnh@gmail.com>
2020-01-27 14:35:22 -0500

    required express and handlebars

--- controllers/items_controller.js
+++ controllers/items_controller.js
@@ -4,4 +4,10 @@ const toDoList = require("../models/todolist");
 
 const router = express.Router();
 
+// get all of the items, and display them on the page
+
+// create a new item
+
+// delete an item
+
 module.exports = router;
